0d7151a5a097af1c8402c0e9d1fe31b658152930
KasunJayasanka <officialblackknight@gmail.com>
2023-07-10 14:38:49 +0530

    Initial Commit

---
+++ lib/model/todo.dart
@@ -0,0 +1,21 @@
+class ToDo{
+  String?id;
+  String?todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone=false
+  });
+
+  static List<ToDo> toDoList(){
+    return[
+      ToDo(id: '01', todoText: 'Morning Exercise',isDone: true),
+      ToDo(id: '02', todoText: 'Morning E',isDone: true),
+      ToDo(id: '03', todoText: 'Morning Exercise',),
+      ToDo(id: '04', todoText: 'Morning Exercise',isDone: true),
+      ToDo(id: '05', todoText: 'Morning Exercise',isDone: true),
+    ];
+  }
+}
